5fc00f699fa99293f0bd2929a12748487cb9a7cc
Caitlin <ccdooley94@gmail.com>
2021-02-12 10:05:23 -0600

    navbar added

--- fuel-quote-app/src/App.js
+++ fuel-quote-app/src/App.js
@@ -1,4 +1,4 @@
-
+import { Nav, Navbar } from 'react-bootstrap'
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,6 +10,19 @@ import QuoteForm from './components/QuoteForm'
 const App = () => {
   return (
       <div className="App">
+          <Navbar bg='light' variant='light' sticky='top' expand='xl'>
+            <Navbar.Text className='pr-3 ml-auto'>USERNAME</Navbar.Text>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                  <Nav className='ml-auto'>
+                      <Nav.Link className='ml-auto' href='#home'>Home</Nav.Link>
+                      <Nav.Link className='ml-auto' href='#quoteform'>Get Quote</Nav.Link>
+                      <Nav.Link className='ml-auto' href='#home'>Quote History</Nav.Link>
+                      <Nav.Link className='ml-auto' href='#home'>Account Details</Nav.Link>
+
+                  </Nav>
+              </Navbar.Collapse>
+          </Navbar>
         <h1>Testing Page</h1>
         <Router>
           <ul>

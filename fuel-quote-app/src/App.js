9f5fb3b187932a8d4bd1b00e0e9fb25f6123a6d6
Caitlin <ccdooley94@gmail.com>
2021-02-12 10:31:03 -0600

    quote form input validation added

--- fuel-quote-app/src/App.js
+++ fuel-quote-app/src/App.js
@@ -1,43 +1,43 @@
-import { Nav, Navbar } from 'react-bootstrap'
+import {Nav, Navbar} from 'react-bootstrap'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from 'react-router-dom'
 import QuoteForm from './components/QuoteForm'
 
 const App = () => {
-  return (
-      <div className="App">
-          <Navbar bg='light' variant='light' sticky='top' expand='xl'>
-            <Navbar.Text className='pr-3 ml-auto'>USERNAME</Navbar.Text>
-              <Navbar.Toggle aria-controls='basic-navbar-nav' />
-              <Navbar.Collapse id='basic-navbar-nav'>
-                  <Nav className='ml-auto'>
-                      <Nav.Link className='ml-auto' href='#home'>Home</Nav.Link>
-                      <Nav.Link className='ml-auto' href='#quoteform'>Get Quote</Nav.Link>
-                      <Nav.Link className='ml-auto' href='#home'>Quote History</Nav.Link>
-                      <Nav.Link className='ml-auto' href='#home'>Account Details</Nav.Link>
+    return (
+        <div className="App">
+            <Navbar bg='light' variant='light' sticky='top' expand='xl'>
+                <Navbar.Text className='pr-3 ml-auto'>USERNAME</Navbar.Text>
+                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='ml-auto'>
+                        <Nav.Link className='ml-auto' href='#home'>Home</Nav.Link>
+                        <Nav.Link className='ml-auto' href='#quoteform'>Get Quote</Nav.Link>
+                        <Nav.Link className='ml-auto' href='#home'>Quote History</Nav.Link>
+                        <Nav.Link className='ml-auto' href='#home'>Account Details</Nav.Link>
 
-                  </Nav>
-              </Navbar.Collapse>
-          </Navbar>
-        <h1>Testing Page</h1>
-        <Router>
-          <ul>
-            <li>
-              <Link to="/quoteform">Quote Form</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route path="/quoteform">
-              <QuoteForm />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-  );
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <h1>Testing Page</h1>
+            <Router>
+                <ul>
+                    <li>
+                        <Link to="/quoteform">Quote Form</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/quoteform">
+                        <QuoteForm/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- fuel-quote-app/src/components/QuoteForm.js
+++ fuel-quote-app/src/components/QuoteForm.js
@@ -1,25 +1,45 @@
-import {Button, Form} from 'react-bootstrap'
+import {Button, Form, Row, Col} from 'react-bootstrap'
+import {useState} from 'react'
 const QuoteForm = () => {
+    /*
+    Form Validation https://react-bootstrap.netlify.app/components/forms/#forms-validation-input-group
+    handleSubmit function checks that al required fields contain valid input
+    Form.Control>Feedback prompts user to correct info if necessary
+    Cannot submit form without validation
+    */
+    const [validated, setValidated] = useState(false)
+    const handleSubmit = (e) => {
+        const form = e.currentTarget
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation()
+        }
+        setValidated(true)
+    }
     return (
         <div>
             <h1>Get a Quote:</h1>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 {/*Gallon Request*/}
-                <Form.Group>
+                <Form.Group controlId='validationGallonReq'>
                     <Form.Label>Gallons Requested:</Form.Label>
-                    <Form.Control type='number' placeholder='0' />
+                    <Form.Control required type='number' placeholder='0' />
+                    <Form.Control.Feedback type='invalid'>Please provide a valid number</Form.Control.Feedback>
                 </Form.Group>
 
                 {/*Verify Address*/}
-                <Form.Group>
-                    <p>[This will be the delivery address]</p>
-                    <Form.Check type='checkbox' label='Delivery Address Verified'/>
+                <Form.Group controlId='validationAddress'>
+                    <Row>
+                    <Col><p>[This will be the delivery address]</p></Col>
+                    <Col><Form.Check required type='checkbox' label='Delivery Address Verified' feedback='Please verify address'/></Col>
+                    </Row>
                 </Form.Group>
 
                 {/*Delivery Date*/}
-                <Form.Group>
+                <Form.Group controlId='validationDelivDate'>
                     <Form.Label>Delivery Date: </Form.Label>
-                    <Form.Control type='date' />
+                    <Form.Control required type='date' />
+                    <Form.Control.Feedback type='invalid'>Please select a delivery date</Form.Control.Feedback>
                 </Form.Group>
 
                 <Button variant='primary' type='submit'>Calculate</Button>

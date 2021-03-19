2f700973c4db796cc7e13753cf9595215e0ff29e
Edward Vo <edwardvo2000@gmail.com>
2021-03-18 22:29:28 -0500

    added axios to Login

--- fuel-quote-app/src/components/Login.js
+++ fuel-quote-app/src/components/Login.js
@@ -2,12 +2,17 @@ import { useHistory } from "react-router-dom";
 import {Container, Button, Form, Row, Col} from 'react-bootstrap'
 import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import axios from 'axios';
+
 
 function simulateNetworkRequest() {
     return new Promise((resolve) => setTimeout(resolve, 100));
   }
 const Login = () => {
     const [isLoading, setLoading] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     let history=useHistory();
 
     useEffect(() => {
@@ -24,13 +29,27 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
+    event.preventDefault();
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
-    }
-    setValidated(true);
-    if (form.checkValidity() == true) history.push("/welcome");
+    } else {     
+      setValidated(true);
 
+      const userObj = {
+        username: username,
+        password: password
+      }
+                                            /* CHANGE /create TO /update */
+      axios.post('http://localhost:4000/users/create', userObj)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+            console.log(error)
+        });
+
+      history.push("/profilemanagement");
+    }
   }
   
     return (
@@ -48,12 +67,16 @@ const Login = () => {
                 <Col md="5"></Col>
             </Row>
 
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form noValidate validated={validated}
+                  onSubmit={handleSubmit}>
             <Row>
                 <Col md="4"></Col>
                 <Col md="4">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" required/>
+                    <Form.Control type="text" 
+                                  value={username} 
+                                  onChange={(e) => setUsername(e.target.value)}
+                                  required/>
                     <Form.Control.Feedback type="invalid">Please provide a username</Form.Control.Feedback>
                 </Col>
             </Row>
@@ -61,7 +84,10 @@ const Login = () => {
                 <Col md="4"></Col>
                 <Col>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required/>
+                    <Form.Control type="password" 
+                                  value={password}
+                                  onChange={(e) => setPassword(e.target.value)}
+                                  required/>
                     <Form.Control.Feedback type="invalid">Please provide a password</Form.Control.Feedback>
                 </Col>
                 <Col md="4"></Col>

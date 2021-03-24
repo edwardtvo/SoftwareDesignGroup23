f0794cf1fb828d2dbc07dc3a1e32c0f3d5cb270d
Caitlin <ccdooley94@gmail.com>
2021-03-24 14:35:15 -0500

    removed redundant code

--- fuel-quote-app/server/models/user-schema.js
+++ fuel-quote-app/server/models/user-schema.js
@@ -2,24 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 // https://mongoosejs.com/docs/populate.html
 
-let quoteSchema = new Schema({
-    //_id: {type: mongoose.Schema.Types.ObjectId},
-    gallons_requested: Number,
-    delivery_date: {type: Date},
-    delivery_address: {type: String}
-}, {
-    collection: 'quotes'
-});
-let historySchema = new Schema({
-    //_id: {type: mongoose.Schema.Types.ObjectId},
-    gallons_requested: Number,
-    delivery_address: {type: String},
-    delivery_date: {type: Date},
-    price_per_gallon: Number,
-    amount_due: Number
-}, {
-    collection: 'history'
-});
 let userSchema = new Schema({
     //_id: mongoose.Schema.Types.ObjectId,
     username: {
@@ -27,7 +9,7 @@ let userSchema = new Schema({
         required: true,
         unique: true,
         validate: { validator: function (i) {
-            var regex = /[a-zA-Z0-9\.\-\'\_]{6,30}$/;
+            const regex = /[a-zA-Z0-9.\-'_]{6,30}$/;
             return regex.test(i);
         }, message: 'Please provide a proper username with at least 6 characters' }
     },
@@ -41,7 +23,7 @@ let userSchema = new Schema({
         //required: true,
         validate: {
             validator: function (i) {
-                var regex = /[A-Za-z0-9\.\-\'\s]{1,100}$/;
+                const regex = /[A-Za-z0-9.\-'\s]{1,100}$/;
                 return regex.test(i);
             }, message: 'Please enter a real full name'
         }
@@ -51,7 +33,7 @@ let userSchema = new Schema({
         //required: true,
         validate: {
             validator: function (i) {
-                var regex = /[A-Za-z0-9\.\-\'\,#\s]{1,150}$/;
+                const regex = /[A-Za-z0-9.\-',#\s]{1,150}$/;
                 return regex.test(i);
             }, message: 'Address is required'
         }
@@ -64,7 +46,7 @@ let userSchema = new Schema({
         //required: true,
         validate: {
             validator: function (i) {
-                var regex = /[A-Za-z]{1,100}$/;
+                const regex = /[A-Za-z]{1,100}$/;
                 return regex.test(i);
             }, message: 'City is required'
         }
@@ -78,32 +60,18 @@ let userSchema = new Schema({
         //required: true,
         validate: {
             validator: function (i) {
-                var regex = /[0-9]{5,9}$/;
+                const regex = /[0-9]{5,9}$/;
                 return regex.test(i);
             }, message: 'Please enter a valid zip code'
         }
     },
-    quoteInfo: [{
+    quotes: [{
         gallons_requested: {type: Number},
         delivery_date: {type: Date}
     }]
 }, {
     collection: 'users'
 });
-
-var usernameValidator = [
-    { validator: function (i) {
-        var regex = /[a-zA-Z0-9\.\-\'\_]{6,30}$/;
-        return regex.test(i);
-    }, message: 'Please provide a proper username with at least 6 characters' }
-
-    /*{ validator: function(v, cb) {
-        User.find({name: v}, function(err,docs){
-           cb(docs.length == 0);
-        });
-      },
-      message: 'User already exists!' } */
-]
 
 // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 //module.exports = mongoose.model('User', userSchema);

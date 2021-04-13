b4b746c7bbc93e1f36f54e7e4633f065bd6f54dd
hvhua <77638872+hvhua@users.noreply.github.com>
2021-04-13 15:15:23 -0500

    Update History.js

--- fuel-quote-app/src/components/History.js
+++ fuel-quote-app/src/components/History.js
@@ -13,7 +13,7 @@ import * as ReactBootstrap from 'react-bootstrap'
 const History = () => {
     const [userList, setUserList] = useState([])
     const componentDidMount = () => {
-        axios.get('http://localhost:4000/users')
+        axios.get('http://localhost:4000/history')
             .then(res => {
                 setUserList(res.data);
                 console.log(userList)
